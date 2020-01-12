557116f74922735da1a5b38ede686bcd16ce8804
Veltoc <veltoc.tech@outlook.com>
2020-01-12 13:29:23 -0800

    Assignment1 complete

---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    public class User
+    {
+        
+        public User(int id, string firstName, string lastName, List<Gift> gifts)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Gifts = gifts;
+            // new List<Gift>();
+
+        }
+        public int Id { get; }
+        private string _FirstName = "<Invalid>";
+        public string FirstName
+        {
+            get => _FirstName;
+            set => _FirstName = string.IsNullOrWhiteSpace(value) ? throw new ArgumentNullException(nameof(value)) : value;
+        }
+        private string _LastName = "<Invalid>";
+        public string LastName
+        {
+            get => _LastName;
+            set => _LastName = string.IsNullOrWhiteSpace(value) ? throw new ArgumentNullException(nameof(value)) : value;
+        }
+        public List<Gift> Gifts { get;} // got a warning to make this read only.
+    }
+
+}
