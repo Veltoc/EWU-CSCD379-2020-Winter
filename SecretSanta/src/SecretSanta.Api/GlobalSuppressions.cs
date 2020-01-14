36721224d7e3eb42574ed7bc22d0e8b0f44d2870
Veltoc <veltoc.tech@outlook.com>
2020-01-14 15:21:40 -0800

    fixed suppressions

--- SecretSanta/src/SecretSanta.Api/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Api.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1822:Member Configure does not access instance data and can be marked as static (Shared in VisualBasic)", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Api.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
--- SecretSanta/src/SecretSanta.Business/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1054:Change the type of parameter url of method Gift.Gift(int, string, string, string, User) from string to System.Uri, or provide an overload to Gift.Gift(int, string, string, string, User) that allows url to be passed as a System.Uri object.", Justification = "<Pending>", Scope = "member", Target = "~M:SecretSanta.Business.Gift.#ctor(System.Int32,System.String,System.String,System.String,SecretSanta.Business.User)")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1056:Change the type of property Gift.Url from string to System.Uri.", Justification = "<Pending>", Scope = "member", Target = "~P:SecretSanta.Business.Gift.Url")]
