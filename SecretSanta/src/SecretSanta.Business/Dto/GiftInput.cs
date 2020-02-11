b0db7225c30ec3f7dff1dfdd9da63824ee3796da
Veltoc <veltoc.tech@outlook.com>
2020-02-10 18:58:41 -0800

    fixed broken unit test

--- SecretSanta/src/SecretSanta.Business/Dto/GiftInput.cs
+++ SecretSanta/src/SecretSanta.Business/Dto/GiftInput.cs
@@ -12,6 +12,6 @@ namespace SecretSanta.Business.Dto
         public string? Description { get; set; }
         public string? Url { get; set; }
         [Required]
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
     }
 }
